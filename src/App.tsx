5623527297abe0f2f4bdc40e7d1fb6dbe33bf596
OmAmberkar <omamberkar08@gmail.com>
2025-12-01 20:12:04 +0530

    feat: starting off with the project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <div className='bg-gray-900 text-white h-screen flex items-center justify-center text-4xl font-extrabold'>
+      <h1>GDGC Vacation project</h1>
+    </div>
+  )
+}
+
+export default App
